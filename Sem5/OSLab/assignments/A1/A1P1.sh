094bd83170761555ee7900a2364d5a0511223302
Prasenjit Kumar Shaw <shawprasenjit07@gmail.com>
2019-09-01 21:10:51 +0530

    Modified OSLab A1P1

--- Sem5/OSLab/assignments/A1/A1P1.sh
+++ Sem5/OSLab/assignments/A1/A1P1.sh
@@ -1,33 +1,43 @@
-echo "Enter userv1:"
-read userv1
+while true
+do
+    echo "========================================"
+    echo "Enter userv1:"
+    read userv1
 
-if [ $userv1 -eq $userv1 2>/dev/null ]; then
-    echo "userv1=$userv1"
-else
-    echo "Non-integer not allowed"
-    exit 1
-fi
+    if [ $userv1 -eq $userv1 2>/dev/null ]; then
+        echo "  userv1=$userv1"
+    else
+        echo "*** Non-integer not allowed, Exiting ***"
+        exit 1
+    fi
 
-echo "Enter userv2:"
-read userv2
+    echo "Enter userv2:"
+    read userv2
 
-if [ $userv2 -eq $userv2 2>/dev/null ]; then
-    echo "userv2=$userv2"
-else
-    echo "Non-integer not allowed"
-    exit 1
-fi
+    if [ $userv2 -eq $userv2 2>/dev/null ]; then
+        echo "  userv2=$userv2"
+    else
+        echo "*** Non-integer not allowed, Exiting ***"
+        exit 1
+    fi
 
-sum=$((userv1+userv2))
-echo "sum:$sum"
+    sum=$((userv1+userv2))
+    echo "SUM : $sum"
 
-mult=$((userv1*userv2))
-echo "mult:$mult"
+    mult=$((userv1*userv2))
+    echo "MULTIPLICATION : $mult"
 
-if [ 0 -eq $userv2 2>/dev/null ]; then
-    echo "Division by zero error"
-    exit 1
-else
-    div=$((userv1/userv2))
-    echo "div:$div"
-fi
+    if [ 0 -eq $userv2 2>/dev/null ]; then
+        echo "*** Division by zero error, Exiting ***"
+        exit 1
+    else
+        div=$((userv1/userv2))
+        echo "DIVISION : $div"
+    fi
+
+    echo "Want to try again? y/n :"
+    read again
+    if [ "$again" == "n" ]; then
+        exit 1
+    fi
+done
